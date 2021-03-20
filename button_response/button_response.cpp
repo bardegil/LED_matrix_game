4919dd69a4ca3f1fc02e5bc7bda59c5f75eac79e
Hans Fjeld <hanse.fjeld@gmail.com>
2021-03-20 18:05:51 +0100

    Added freertos task

---
+++ button_response/button_response.cpp
@@ -0,0 +1,93 @@
+/*
+ * Example of a Arduino interruption and RTOS Binary Semaphore
+ * https://www.freertos.org/Embedded-RTOS-Binary-Semaphores.html
+ */
+
+
+/*
+ * Declaring a global variable of type SemaphoreHandle_t 
+ * 
+ */
+#include "button_response.h"
+#include <cstdio>
+
+class button_response
+{
+private:
+  TaskHandle_t task;
+  SemaphoreHandle_t btn_sem;
+  interrupt_response_function_t callback;
+  /* data */
+public:
+  button_response(/* args */);
+  ~button_response();
+  TaskLed(void * pvArgs);
+  interruptHandler();
+};
+
+
+
+button_response::button_response(int button_pin,interrupt_response_function_t callback_function)
+{
+  char buf[20]
+  sprintf(buf,"led_%d_task",button_pin);
+  pinMode(button_pin, INPUT_PULLUP);
+   // Create task for Arduino led 
+  btn_sem = xSemaphoreCreateBinary();
+  if (interruptSemaphore != NULL) {
+    // Attach interrupt for Arduino digital pin
+    attachInterrupt(digitalPinToInterrupt(button_pin), interruptHandler, LOW);
+  }
+  callback = callback_function;
+  configASSERT(callback != NULL);
+  task = xTaskCreate(TaskLed, // Task function
+              buf, // Task name
+              128, // Stack size 
+              NULL, 
+              4, // Priority
+              NULL );
+  configASSERT(task != NULL);  
+}
+
+button_response::~button_response()
+vTaskDelete()
+{
+}
+
+
+
+void button_response::interruptHandler() {
+  /**
+   * Give semaphore in the interrupt handler
+   * https://www.freertos.org/a00124.html
+   */
+  BaseType_t wakup = pdFALSE; 
+  configASSERT(xSemaphoreGiveFromISR(btn_sem, &wakup) == pdPASS);
+  if(wakup){
+    portYIELD_FROM_ISR();
+  }
+
+}
+
+
+/* 
+ * Led task. 
+ */
+void button_response::TaskLed(void *pvParameters)
+{
+  (void) pvParameters;
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  for (;;) {
+    
+    /**
+     * Take the semaphore.
+     * https://www.freertos.org/a00122.html
+     */
+    if (xSemaphoreTake(interruptSemaphore, portMAX_DELAY) == pdPASS) {
+      callback();
+    }
+    vTaskDelay(10);
+  }
+}
---
+++ button_response/button_response.h
@@ -0,0 +1,8 @@
+// Include Arduino FreeRTOS library
+#include <Arduino_FreeRTOS.h>
+
+// Include semaphore supoport
+#include <semphr.h>
+typedef void (* interrupt_response_function_t)();
+
+/* 
